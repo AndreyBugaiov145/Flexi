2ee3faf112a1c1eb7d03e8919a0a959a1f2e7b6b
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-29 11:18:31 +0300

    task6

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<span>fsdfsdfsddf</span>
+	<div class="tasks">
+		<h1>Список заданий</h1>
+		<ul>
+			<li><a href="task4.php">task4</a></li>
+			<li><a href="task6.php">task6</a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+		</ul>
+	</div>
+	<script src="js/js.js"></script>
 </body>
 </html>
---
+++ task6.php
@@ -0,0 +1,11 @@
+<?php
+function getNumber($a,$b)
+{	
+	(//$a-$b)?
+}
+if (-5) {
+	echo "-5";
+	exit;
+}
+echo"dont wirk";
+?>
