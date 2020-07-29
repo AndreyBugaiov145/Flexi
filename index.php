54d0c9e437ec42b31c7118270693f78cf28fb3fd
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-29 09:41:27 +0300

    new index

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "<h1 style=\"text-align:center;margin:15px;font-size:40px;\">Site path: <span style=\"color:red\">/var/www/html/AndreyBugaiov</span></h1><hr>"; phpinfo() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<span>fsdfsdfsddf</span>
+</body>
+</html>
