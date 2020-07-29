62d959a82e7234976251d9c125d4fefb814ded71
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-29 16:22:31 +0300

    GU-653 Task10 update

--- serverTask10.php
+++ serverTask10.php
@@ -1,6 +1,6 @@
 <?php
 	//масив тайм зон
-	$arrayTimeZone = array("Asia/Tokyo","Asia/Hong_Kong","Europe/Moscow","Europe/Kiev","	Europe/Minsk","Europe/Paris","Europe/Riga","Europe/Rome","Europe/Berlin,Europe/Lisbon");
+	$arrayTimeZone = array("Asia/Tokyo","Asia/Hong_Kong","Europe/Moscow","Europe/Kiev","	Europe/Minsk","Europe/Paris","Europe/Riga","Europe/Rome","Europe/Berlin","Europe/Lisbon");
 	//получение тела fetch  запроса
 	$exploded =json_decode(file_get_contents('php://input'));
 	$el = $exploded->check;
