ce6ae81aad1fb191e08aadf1e95d24f177e98f2b
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-30 09:32:47 +0300

    GU-651-#8 - Создать форму

--- task8.php
+++ task8.php
@@ -12,13 +12,30 @@
 		<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post" class="form1">
 			<label for="name">Введите ваше имя</label><input type="text" name="name" id="name"><br>
 			<label for="password">Введите пароль</label><input type="password" name="password" id="password">
-			<input type="submit" name="submit">
+			<input type="submit" name="submit"><br>
+			<span>Отметь свой skill</span>		
+			<label for="password">Css</label>
+			<input type="checkbox"  name="checkbox[]" value="Css" id="Css">
+			<label for="JS">JS</label>
+			<input type="checkbox" name="checkbox[]"  value="JS" id="JS">
+			<label for="PHP">PHP</label>
+			<input type="checkbox" name="checkbox[]"  value="PHP" id="PHP"><br>
+			<span>Выбери пол</span>
+			<select name="select" id="select">
+				<option value="Мужской">Мужской</option>
+				<option value="Женский">Женский</option>
+			</select>
 		</form>
+		<br><hr><br>
 		<?php if(isset($_POST["submit"])):?>
 			<div>
 				<span>Ваше имя : <?=$_POST["name"]?></span>
-				<br>
+				
 				<span>Ваш пороль : <?=$_POST["password"]?></span>
+				<span>Ваш Skill : <? foreach ($_POST["checkbox"] as $value) {
+					echo $value." ,";
+				}?></span>
+				<span>Ваш пол : <?=$_POST["select"]?></span>
 			</div>
 		<?php endif;?>
 	</div>
