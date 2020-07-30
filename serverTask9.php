683f1d503f1db39f608a8a4259a10a65453b6ebf
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-30 10:54:49 +0300

    GU-652-#9 - Создать файл и записать в него текст-update4

--- serverTask9.php
+++ serverTask9.php
@@ -1,17 +1,17 @@
 <?php
 	$file = fopen("text.txt","a+");
 	fwrite($file,$_POST['text']);
-	
-	while (!feof($file)) {
-		echo fread($file,2);
-	}
+	echo fread($file,500);
+	/*while (!feof($file)) {
+		echo fread($file,500);
+	}*/
 	fclose($file);
+
 //               1
 	/*file_put_contents("text.txt",$_POST['text']);
 	echo file_get_contents("text.txt");*/
 
 	//unlink("text.txt");
-
 
 
 ?>
