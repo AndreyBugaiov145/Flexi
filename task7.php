8319f54e6c772b52146f6a735a9ab1f780cac750
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-07-31 18:03:30 +0300

    GU-649-#6 - Написать функцию- Update

--- task7.php
+++ task7.php
@@ -4,6 +4,7 @@ function FunctionName()
 	$rez =true;
 	$type=null;
 	$args = func_get_args ();
+	$ansver;
 
 	foreach ($args as $val ) {
 	$type = $type?$type : gettype($val);
@@ -11,15 +12,15 @@ function FunctionName()
 	}
 
 	if($rez){
-		echo "Все хорошо типы данных совпадают<br>";
+		$ansver="Все хорошо типы данных совпадают<br>";
 	}else{
 		for ($i=0; $i < count($args); $i++) { 
 			//$args[$i] .="";
 			$args[$i]=strval($args[$i]);
 		}
-		echo "Ошибка .Типы данных были приведены к строчному типу<br>";
+		$ansver= "Ошибка .Типы данных были приведены к строчному типу<br>";
 	}
-	return $args;
+	return $ansver.$args;
 }
 var_dump(FunctionName(15,"2",87));
 
