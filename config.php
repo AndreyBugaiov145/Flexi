cb99ee1ad50f291714a1917346a97f7e082591c0
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 11:16:45 +0300

    conf

--- config.php
+++ config.php
@@ -9,24 +9,7 @@
 	$login = 'root';
 	$password = 'flexi140';
 	$bd = 'tasks';
+	
 */
-/*
-	$misqli = new mysqli($host, $login ,$password,$bd );
-	//$misqli = new mysqli('91.239.235.118', 'bugaiovandrey' ,'sdfsdfsklk3333','test' );
-	$misqli->query("SET NAMES 'utf8'");
 
-	$sucses = $misqli->query("SELECT * FROM `Product` ORDER BY product "); 
-	function get($ar)
-	{	$arr = array();
-		while(($row = $ar->fetch_assoc())!=false){
-			$arr[] = $row;
-		}
-		return $arr;
-	}
-		 $arr =get($sucses);
-		 var_dump($arr);
-
-	$misqli->close();
-
-	*/
 ?>
