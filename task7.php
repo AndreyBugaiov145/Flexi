16f5b74511125ac7568526c10e6801f8666eb08b
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 09:34:33 +0300

    GU-650-#7 - Создать функцию

--- task7.php
+++ task7.php
@@ -4,24 +4,20 @@ function FunctionName()
 	$rez =true;
 	$type=null;
 	$args = func_get_args ();
-	$ansver;
 
 	foreach ($args as $val ) {
 	$type = $type?$type : gettype($val);
 	$rez = ($type===gettype($val)&&$rez)?true :false;
 	}
 
-	if($rez){
-		$ansver="Все хорошо типы данных совпадают<br>";
-	}else{
+	if(!$rez){
 		for ($i=0; $i < count($args); $i++) { 
 			//$args[$i] .="";
 			$args[$i]=strval($args[$i]);
 		}
-		$ansver= "Ошибка .Типы данных были приведены к строчному типу<br>";
 	}
-	return $ansver.$args;
+	return $args;
 }
-var_dump(FunctionName(15,"2",87));
+var_dump(FunctionName(15,4,87));
 
 ?>
