899ded125c154dddc5ea6dc8938c16f07de1394d
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 16:04:06 +0300

    GU-659-#16 - Datepicker

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
 			<li><a href="task9.php">task9</a></li>
 			<li><a href="task10.php">task10</a></li>
 			<li><a href="task11.php">task11</a></li>
-			<li><a href="task12.php">task12(не могу подключиться к PhpMyAdmin по этому работает только на локальном сервере)</a></li>
+			<li><a href="task12.php">task12</a></li>
 			<li><a href="task13.php">task13</a></li>
 			<li><a href="task14.php">task14</a></li>
-			<li><a href="task15.php">task15`</a></li>
-			<li><a href="#"></a></li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptas ad tempora officia. Quam est illum quos, molestiae iusto nobis?
+			<li><a href="task15.php">task15</a></li>
+			<li><a  href="task16.php">task16</a></li>
 		</ul>
 	</div>
 	<script src="js/js.js"></script>
---
+++ task16.php
@@ -0,0 +1,55 @@
+<?php
+if ($_GET['submit']){
+  require 'config.php';
+  $misqli = new mysqli($host, $login ,$password,$bd );
+  $misqli->query("SET NAMES 'utf8'");
+	$sucses = $misqli->query("SELECT `name` , `date` FROM `task16` WHERE  `date`>'{$_GET['date_from']}' AND `date`<'{$_GET['date_to']}'  "); 
+	function get($ar)
+		{	$arr = array();
+			while(($row = $ar->fetch_assoc())!=false){
+				$arr[] = $row;
+			}
+			return $arr;
+		}
+		$arr =get($sucses);
+	foreach ($arr as  $value) {
+			echo "Имя --".$value['name']."    Датф рождения = ".$value['date']."<br/>";
+		}	
+
+	$misqli->close();
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Datepicker - Default functionality</title>
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="https://jqueryui.com/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  	$( function() {
+    $( "#date_from" ).datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+     $( "#date_to" ).datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+  });  
+  
+  </script>
+</head>
+<body>
+ <form action="" method="get">
+ 	<p>Date: <input type="text" id="date_from" name="date_from" ></p>
+ 	<p>Date: <input type="text" id="date_to" name="date_to" ></p>
+ 	<input type="submit" name="submit">
+ </form>
+
+</span> 
+ 
+</body>
+</html>
