8da3683d9fdd882f65eea84fd8db5c8531b8ebf3
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 17:17:22 +0300

    GU-660-#17 - Выполнить следующую последовательность

---
+++ serverTask17.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_FILES['file'])){
+	$errors = array();
+	$file_name = $_FILES['file']["name"];
+	$file_size = $_FILES['file']["size"];
+	$file_tmp = $_FILES['file']["tmp_name"];
+	$file_type = $_FILES['file']["type"];
+	$file_ext = strtolower(end(explode(".", $_FILES['file']["name"])));
+
+	$expensions= array("jpeg","png","jpg");
+
+	echo "<br> file_name =  ".$file_name ;
+	echo "<br> file_size =  ".$file_size ;
+	echo "<br> file_tmp =  ".$file_tmp ;
+	echo "<br> file_type =  ".$file_type ;
+	echo "<br> file_ext =  ".$file_ext ;
+	echo "<br>";
+	echo "<br>";
+	print_r($_FILES['file']);
+	
+	move_uploaded_file($file_tmp, $file_name);
+
+}
+
+var_dump($_FILES['file']);
+
+?>
---
+++ task17.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>task17</title>
+</head>
+<body>
+	<form action="serverTask17.php" method="post" enctype="multipart/form-data">
+		<label > Добавить файл<input type="file" name="file" ></label>
+		<input type="submit">
+	</form>	
+</body>
+</html>
