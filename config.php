3e8b1e176b4c702f751030d64395448c6a834af0
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 10:23:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@
 	$bd = 'tasks';
 */
 
-	$misqli = new mysqli($host  , $login  ,$password,$bd );
+	$misqli = new mysqli('91.239.235.118', 'bugaiovandrey' ,'sdfsdfsklk3333','test' );
 	$misqli->query("SET NAMES 'utf8'");
 /*
 	$sucses = $misqli->query("SELECT * FROM `Product` ORDER BY product "); 
