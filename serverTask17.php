70319c7ec342e7223d86c5ab45bd02482ba1f1cb
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 17:41:23 +0300

    GU-660-#17 - Выполнить следующую последовательность

--- serverTask17.php
+++ serverTask17.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_FILES['file'])){
-	$img = "img";
+	$img = "load";
 	$file_name = $_FILES['file']["name"];
 	$file_size = $_FILES['file']["size"];
 	$file_tmp = $_FILES['file']["tmp_name"];
@@ -10,9 +10,9 @@ if(isset($_FILES['file'])){
 	move_uploaded_file($file_tmp, "$img/$file_name");
 
 	if ((explode("/",$file_type ))[0] === 'image') {
-		echo "<img src='img/{$file_name}'>";
+		echo "<img style='width:300px,height:300px' src='load/{$file_name}'>";
 	}else{
-		echo "string";
+		echo "<a href='load/{$file_name}' download>Скачать файл</a>";
 	}
 }
 
