ae9094a17cb1262ae1b57f7d3d911df59d5b8935
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-10 17:35:08 +0300

    GU-660-#17 - Выполнить следующую последовательность

--- serverTask17.php
+++ serverTask17.php
@@ -1,30 +1,19 @@
 <?php
 
 if(isset($_FILES['file'])){
-	$errors = array();
+	$img = "/img";
 	$file_name = $_FILES['file']["name"];
 	$file_size = $_FILES['file']["size"];
 	$file_tmp = $_FILES['file']["tmp_name"];
 	$file_type = $_FILES['file']["type"];
-	//$file_ext = strtolower(end(explode(".", $_FILES['file']["name"])));
 
-	$expensions= array("jpeg","png","jpg");
+	move_uploaded_file($file_tmp, "$img/$file_name");
 
-	echo "<br> file_name =  ".$file_name ;
-	echo "<br> file_size =  ".$file_size ;
-	echo "<br> file_tmp =  ".$file_tmp ;
-	echo "<br> file_type =  ".$file_type ;
-	echo "<br> file_ext =  ".$file_ext ;
-	echo "<br>";
-	echo "<br>";
-	print_r($_FILES['file']);
-
-	move_uploaded_file($file_tmp, $file_name);
 	if ((explode("/",$file_type ))[0] === 'image') {
 		echo "<img src='{$file_name}'>";
+	}else{
+		echo "string";
 	}
 }
-
-var_dump($_FILES['file']);
 
 ?>
