6a2794bbebffc4ce7859c03ce9fdfd2b3f33935a
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-11 17:26:19 +0300

    GU-659-#16 - Datepicker update

--- task16.php
+++ task16.php
@@ -1,11 +1,15 @@
 <?php
 if (isset($_GET['submit'])){
   require 'config.php';
-  $date_from = "0-".$_GET['date_from'];
-  $date_to = "99999-".$_GET['date_to'];
+  $date_from = $_GET['date_from'];
+  $date_to = $_GET['date_to'];
+  if($date_from>$date_to){
+    echo "неверный выбор даты<a href='task16.php'> попробовать снова<a>";
+    exit;
+  }
   $misqli = new mysqli($host, $login ,$password,$bd );
   $misqli->query("SET NAMES 'utf8'");
-	$sucses = $misqli->query("SELECT `name` , `date` FROM `task16` WHERE  `date`>'{$date_from}' AND `date`<'{$date_to}'"); 
+  $sucses = $misqli->query("SELECT `name` , `date` FROM `task16` "); 
 	function get($ar)
 		{	$arr = array();
 			while(($row = $ar->fetch_assoc())!=false){
@@ -13,9 +17,28 @@ if (isset($_GET['submit'])){
 			}
 			return $arr;
 		}
-		$arr =get($sucses);
+	$arr =get($sucses);
+  function cheackData($date,$date_from,$date_to)
+  {
+    $date = explode("-",$date);
+    $date_from = explode("-",$date_from);
+    $date_to = explode("-",$date_to);
+  
+    if($date[1]>$date_from[0]&&$date[1]<$date_to[0]){
+        return true;
+    }else if(($date[1]===$date_from[0]&&$date_from[0]===$date_to[0]&&$date[2]>$date_from[1]&&$date[2]<$date_to[1])||($date[1]===$date_from[0]&&$date_from[0]!=$date_to[0]&&$date[2]>$date_from[1])){
+        return true;
+    }else if($date[1]===$date_to[0]&&$date[2]<$date_to[1]){
+      return true;
+    }
+    return false;
+  }
 	foreach ($arr as  $value) {
-			echo "Имя --".$value['name']."//   Датa рождения = ".$value['date']."<br/>";
+
+    if (cheackData($value['date'],$date_from,$date_to)) {
+     echo "Имя --".$value['name']."//   Датa рождения = ".$value['date']."<br/>";
+    }
+			
 		}	
 
 	$misqli->close();
@@ -27,31 +50,37 @@ if (isset($_GET['submit'])){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Datepicker - Default functionality</title>
-  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <title>Datepicker </title>
+   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="https://jqueryui.com/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
+  <style>
+    .ui-datepicker-year{
+       display:none;   
+      }
+  </style>
   <script>
-  	$( function() {
+    $( function() {
     $( "#date_from" ).datepicker({
-        dateFormat: 'mm-dd'
+        dateFormat: 'mm-dd',
+        changeYear: false 
     });
      $( "#date_to" ).datepicker({
-        dateFormat: 'mm-dd'
+        dateFormat: 'mm-dd',
+        changeYear: false 
     });
   });  
   
-  </script>
+  </script> 
 </head>
 <body>
  <form action="" method="get">
  	<p>Date: <input type="text" id="date_from" name="date_from" ></p>
- 	<p>Date: <input type="text" id="date_to" name="date_to" ></p>
+ 	<p>Date: <input type="text" id="date_to" name="date_to"></p>
  	<input type="submit" name="submit">
  </form>
 
-</span> 
  
 </body>
 </html>
