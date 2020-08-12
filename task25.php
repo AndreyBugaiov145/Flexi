680143d077d5b0b94e1bca3341eb0d7bea633f11
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-12 17:30:54 +0300

    GU-669#25 - Подключение google maps

---
+++ task25.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<iframe src="https://www.google.com/maps/d/embed?mid=1JQPdDMXS8mYiumKLCvp1RHQklvEoR9qM" width="640" height="480"></iframe>
+	
+</body>
+</html>
