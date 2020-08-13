a45de4dd916f1090ebd7ab7a8cdd5ef6c2936102
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-13 09:56:07 +0300

    GU-669-#25 - Подключение google maps

--- task25.php
+++ task25.php
@@ -3,9 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	 <style>
+
+      #map {
+        height: 400px;  
+        width: 600px;  
+       }
+    </style>
 </head>
 <body>
 	<iframe src="https://www.google.com/maps/d/embed?mid=1JQPdDMXS8mYiumKLCvp1RHQklvEoR9qM" width="640" height="480"></iframe>
-	
+
+<!-- d
+    <div id="map"></div>
+    <script>
+		function initMap() {
+			var opt = {lat: 47.836642, lng: 35.106410};
+			var map = new google.maps.Map(
+			document.getElementById('map'), {zoom: 15, center: opt});
+			var marker = new google.maps.Marker({position: opt, map: map});
+		}
+    </script>
+
+    <script defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUcbC8FSKBFKUUoE9ACLIGE-vFUeQbjUg&callback=initMap">
+    </script>
+ -->
+
 </body>
 </html>
