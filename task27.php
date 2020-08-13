d605006633266b1d8d13e6870319825008c5429a
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-13 12:06:17 +0300

    GU-671-#27 - Создать простую страницу в которой будет два блока с текстом и кнопкой load more

---
+++ task27.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>task27</title>
+	<style>
+		.content{
+			float: left;
+			width: 40%;
+			margin: 3%;
+			padding: 15px;
+			height: 100%;
+			border: 1px solid silver;
+			border-radius: 10px;
+		}
+		div: {
+		  display: none;
+		}
+		.moretext{
+			display: none;
+		}
+		.btn:focus~span{
+			display: block;
+		}
+		hr{
+			margin: 4px;
+		}
+	</style>
+</head>
+<body>
+	<div class="content">
+		<span class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delectus unde est, ex ad repudiandae asperiores deserunt, vero fuga!</span>
+		<input type="button" class="btn" value="asfsdds">
+		<hr>
+		<span class="moretext">Lorem ipsu ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum del ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delm dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delectus unde est, ex ad repudiandae asperiores deserunt, vero fuga!</span>
+	</div>
+	<div class="content">
+		<span class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delectus unde est, ex ad repudiandae asperiores deserunt, vero fuga!</span>
+		<input type="button" class="btn" value="asfsdds">
+		<hr>
+		<span class="moretext">Lorem ipsum dolor sit amet, co ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum del ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delnsectetur adipisicing elit. Accusamus odio quas, provident asperiores. Magnam, minus, a! Sit nobis rerum illum delectus unde est, ex ad repudiandae asperiores deserunt, vero fuga!</span>
+	</div>
+</body>
+</html>
