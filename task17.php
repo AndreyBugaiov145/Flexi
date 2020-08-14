470fe5d41712d4e78bda7ce30e14ca65d46df68b
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-14 12:54:07 +0300

    GU-660-#17 - Выполнить следующую последовательность

--- task17.php
+++ task17.php
@@ -24,7 +24,7 @@
 			$file_tmp = $_FILES['file']["tmp_name"];
 			$file_type = $_FILES['file']["type"];
 			move_uploaded_file($file_tmp, "$load/$file_name");
-
+			$type= array('jpg','png','jpeg');
 
 		    $dir = "load/";   //задаём имя директории
 		    if(is_dir($dir)) {   //проверяем наличие директории
@@ -32,8 +32,15 @@
 		         $files = scandir($dir);    //сканируем (получаем массив файлов)
 		         array_shift($files); // удаляем из массива '.'
 		         array_shift($files); // удаляем из массива '..'
-		         for($i=0; $i<sizeof($files); $i++) echo '-файл: <a href="'.$dir.$files[$i].'" title="открыть/скачать файл или страницу">'.$files[$i].'</a>;<br>';  //выводим все файлы
+		         for($i=0; $i<sizeof($files); $i++){
+		         	if ((explode(".",$dir.$files[$i] ))[1] === $type[0]||(explode(".",$dir.$files[$i]))[1] === $type[1]||(explode(".",$dir.$files[$i]))[1] === $type[2]) {
+						echo "<img style='width:300px,height:300px' src='load/{$dir.$files[$i]}'>";
+					}else{
+						echo "<a href='load/{$dir.$files[$i]}' download>Скачать файл</a>";
+					
+		         }
 		    } 
+		}
 		    else echo $dir.' -такой директории нет;<br>';
 
 /*
