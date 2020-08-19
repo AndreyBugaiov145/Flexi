6d9a2251062d92bdcd69c5b91f40e0d8697fc334
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-19 11:03:51 +0300

    GU-660-#17 - Выполнить следующую последовательность

--- task17.php
+++ task17.php
@@ -29,26 +29,25 @@
 			$file_tmp = $_FILES['file']["tmp_name"];
 			$file_type = $_FILES['file']["type"];
 			move_uploaded_file($file_tmp, "$load/$file_name");
-			$type= array('jpg','png','jpeg');
-
+			
 		    
 		}
 	}
 	$dir = "load/"; 
-		    if(is_dir($dir)) {   
-
-		         $files = scandir($dir);   
-		         array_shift($files); 
-		         array_shift($files); 
-		         for($i=0; $i<sizeof($files); $i++){
-		         	if ((explode(".",$dir.$files[$i] ))[1] === $type[0]||(explode(".",$dir.$files[$i]))[1] === $type[1]||(explode(".",$dir.$files[$i]))[1] === $type[2]) {
-						echo "<div class='d'><img style='width:300px,height:300px' src='{$dir}{$files[$i]}'></div><br>";
-					}else{
-						echo "<div class='d'><a href='$dir}{$files[$i]}' download>Скачать файл</a></div><br>";
-					
-		         }
-		    } 
-		} else echo $dir.' -такой директории нет;<br>';
+	    if(is_dir($dir)) {   
+	    	$type= array('jpg','png','jpeg');
+	         $files = scandir($dir);   
+	         array_shift($files); 
+	         array_shift($files); 
+	         for($i=0; $i<sizeof($files); $i++){
+	         	if ((explode(".",$dir.$files[$i] ))[1] === $type[0]||(explode(".",$dir.$files[$i]))[1] === $type[1]||(explode(".",$dir.$files[$i]))[1] === $type[2]) {
+					echo "<div class='d'><img style='width:300px,height:300px' src='{$dir}{$files[$i]}'></div><br>";
+				}else{
+					echo "<div class='d'><a href='$dir}{$files[$i]}' download>Скачать файл</a></div><br>";
+				
+	         }
+	    } 
+	} else echo $dir.' -такой директории нет;<br>';
 ?>
 </body>
 </html>
