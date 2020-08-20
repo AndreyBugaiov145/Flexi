e4099c5cf07f4826e436f89ea9c30cc6e6213fa9
Andrey Bugaiov <bugaiov.andrey@gmail.com>
2020-08-20 11:50:37 +0300

    f

--- task32Api.php
+++ task32Api.php
@@ -1,5 +1,4 @@
 <?php
-//echo "strasdsading";
 require 'config.php';
 require 'bdConect.php';
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -21,6 +20,7 @@ $dbh = null;
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
 	<div class="container">
